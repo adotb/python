5a3af7fefbdc99559cc1e4254630812a08d6faf5
massimo <massimo@massimo.hi.inet>
2015-07-23 12:08:45 +0200

    First commit

---
+++ simple_tcp_client.py
@@ -0,0 +1,16 @@
+import socket
+
+host = "www.google.com"
+port = 80
+
+# Create a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the client to the server
+client.connect((host,port))
+
+# Send data
+client.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+
+# Receive data
+print client.recv(4096)
